4ad06e6f29643f800f7d41d7af0ca0ea05b59301
Eduardo de Oliveira Siqueira <eduardo.siqueira@ftt-14L.engcomp.local>
2019-05-21 17:05:37 -0300

    Separando o arquivo de log em linhas, e criando um variavel para cada jogo

--- src/app/log-uploader/log-uploader.component.ts
+++ src/app/log-uploader/log-uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { LineToLineMappedSource } from 'webpack-sources';
 
 @Component({
   selector: 'app-log-uploader',
@@ -7,12 +8,44 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class LogUploaderComponent implements OnInit {
   @ViewChild('logInput') logInput: ElementRef;
+  lines: string[];
 
   constructor() {}
 
   ngOnInit() {}
 
   onLogChange(event) {
-    console.log(event.target.files[0]);
+    const reader = new FileReader();
+
+    reader.readAsText(event.target.files[0]);
+
+    reader.onload = () => this.logResolver(reader.result);
+  }
+
+  logResolver(log) {
+    this.lines = log.split(/[0-9]?[0-9]?[0-9]:[0-9][0-9]/);
+    let gameNumber = 0;
+    let currentGame = [];
+
+    this.lines.forEach(line => {
+      if (line.includes('InitGame:')) {
+        if (gameNumber > 0) {
+          this.createGame(gameNumber, currentGame);
+        }
+        gameNumber++;
+        currentGame = [];
+      }
+
+      currentGame.push(line);
+    });
+
+    // Cria o último jogo que o loop não pega
+    this.createGame(gameNumber, currentGame);
+  }
+
+  createGame(gameNumber: number, game: string[]) {
+    console.log(`GameNumber: ${gameNumber}`);
+    console.log('----');
+    console.log(game);
   }
 }
